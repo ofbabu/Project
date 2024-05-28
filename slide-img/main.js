b0a578928cca5717b33c981c52d71e1f070b312b
Omar Faruk <162047444+ofbabu@users.noreply.github.com>
2024-05-28 20:11:47 +0600

    change

--- slide-img/main.js
+++ slide-img/main.js
@@ -1,8 +1,19 @@
-
+var i = 0;
 var photos = ["./img/img1.jpg","./img/img2.jpg","./img/img3.jpg","./img/img5.jpg"]
- var imgTag = document.querySelector("img");
+var imgTag = document.querySelector("img");
  count = 0;
 
+ //daynamic code
+ function daynamicChange(){
+    document.slide.src = photos[i]
+    if(i < photos.length - 1){
+        i++;
+    }else{
+        i = 0;
+    }
+    setTimeout("daynamicChange()",5000);
+ }
+window.onload = daynamicChange;
  function next() {
     count++;
     if(count >= photos.length){
